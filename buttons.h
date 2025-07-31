3e691c76f0581bb43eb9427ed46523ade1a909e1
桂乃芬 <nwinsthonh@Gmail.com>
2025-07-31 08:52:24 +0700

    add git

--- buttons.h
+++ buttons.h
@@ -2,19 +2,16 @@
 #include "config.h"
 #include "display.h"
 
-// Button pin definitions
 #define BTN_LEFT 25
 #define BTN_SELECT 26
 #define BTN_RIGHT 27
 
-// Button states
 enum ButtonState {
     BTN_RELEASED,
     BTN_PRESSED,
     BTN_HELD
 };
 
-// Menu items
 const String menuItems[] = {
     "Test Finger",
     "Enroll Finger", 
@@ -25,15 +22,13 @@ const String menuItems[] = {
 };
 const int menuItemCount = 6;
 
-// Global variables
 extern int currentMenuItem;
 extern bool inMenu;
 extern unsigned long lastButtonPress;
 extern unsigned long lastMenuUpdate;
 
-// Constants
 const unsigned long debounceDelay = 50;
-const unsigned long menuTimeout = 30000; // 30 seconds menu timeout
+const unsigned long menuTimeout = 30000;
 
 void initButtons();
 ButtonState readButton(int pin, bool &lastState, unsigned long &lastDebounce);
